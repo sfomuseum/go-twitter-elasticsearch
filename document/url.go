f417e71c2db76be09131e5393552f04161cb1137
thisisaaronland <thisisaaronland@localhost>
2020-06-22 13:26:14 -0700

    url unshortening

---
+++ document/url.go
@@ -0,0 +1,69 @@
+package document
+
+import (
+	"context"
+	"fmt"
+	"github.com/sfomuseum/go-url-unshortener"
+	"github.com/tidwall/gjson"
+	"github.com/tidwall/sjson"
+	"log"
+	"time"
+)
+
+var cache unshortener.Unshortener
+
+func init() {
+
+	rate := time.Second / time.Duration(10)
+	timeout := time.Second * time.Duration(30)
+
+	worker, err := unshortener.NewThrottledUnshortener(rate, timeout)
+
+	if err != nil {
+		panic(err)
+	}
+
+	seed := make(map[string]string)
+
+	cache, err = unshortener.NewCachedUnshortenerWithSeed(worker, seed)
+
+	if err != nil {
+		panic(err)
+	}
+}
+
+func AppendUnshortenedURLs(ctx context.Context, body []byte) ([]byte, error) {
+
+	urls_rsp := gjson.GetBytes(body, "entities.urls")
+
+	if !urls_rsp.Exists() {
+		return body, nil
+	}
+
+	for idx, u := range urls_rsp.Array() {
+
+		expanded_rsp := u.Get("expanded_url")
+
+		if !expanded_rsp.Exists() {
+			continue
+		}
+
+		expanded_url := expanded_rsp.String()
+
+		u, err := unshortener.UnshortenString(ctx, cache, expanded_url)
+
+		if err != nil {
+			log.Println(expanded_url, err)
+			continue
+		}
+
+		path := fmt.Sprintf("entities.urls.%d.unshortened_url", idx)
+		body, err = sjson.SetBytes(body, path, u.String())
+
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return body, nil
+}
--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/cenkalti/backoff/v4 v4.0.2
 	github.com/elastic/go-elasticsearch/v7 v7.8.0
 	github.com/sfomuseum/go-flags v0.3.1
+	github.com/sfomuseum/go-url-unshortener v0.1.0
 	github.com/tidwall/gjson v1.6.0
 	github.com/tidwall/sjson v1.1.1
 )
